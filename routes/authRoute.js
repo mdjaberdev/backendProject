408f16ec81ddf710c7435ceff3e323c70c4baf3b
mdjaberdev <mdjaber.dev@gmail.com>
2026-07-25 02:51:14 +0600

    status code add

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/greeting", (req, res) => {
-  res.send("Hello Developer");
+  res.status(200).json("Hello Frontend Developer");
 });
 
 module.exports = router;
