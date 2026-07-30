c1c5157239823f81dd9120aa96b4084a6f81f5a3
mdjaberdev <mdjaber.dev@gmail.com>
2026-07-31 00:11:27 +0600

    aa

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(createLimiter({ limit: 50 }));
 
 // Swagger Route
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use("/api/v1/auth", createLimiter({ limit: 3 }), authRoute);
+app.use("/api/v1/auth", createLimiter({ limit: 20 }), authRoute);
 
 app.listen(5000, () => {
   console.log("Server is running 5000 port");
