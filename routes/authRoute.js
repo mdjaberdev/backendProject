3b8161b5c30e54a5772f51e23ba0410cb6ecd076
mdjaberdev <mdjaber.dev@gmail.com>
2026-08-02 02:22:35 +0600

    storage setup

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,23 +1,11 @@
 const express = require("express");
-const multer = require("multer");
 const otpController = require("../controllers/otpController");
 const loginController = require("../controllers/loginController");
 const logoutController = require("../controllers/logoutController");
 const productsController = require("../controllers/productsController");
 const deleteProductsController = require("../controllers/deleteProductsController");
 const _ = express.Router();
-
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./uploads");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = "img" + "-" + Date.now();
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+const upload = require("../utils/storage");
 
 _.post("/sendotp", otpController);
 _.post("/login/:email", loginController);
